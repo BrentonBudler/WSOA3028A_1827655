50d48c693e6852111fdc148d58b16801505f5a8d
BrentonBudler <60913244+BrentonBudler@users.noreply.github.com>
2020-04-23 10:31:23 +0200

    Add information geography page

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ function includeHTML() {
     }
   }
 }
+
+mybutton = document.getElementById("topButton");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
